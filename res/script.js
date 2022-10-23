5f1c84effc01936a02c1abc69ece3fb55edf4389
Faizal Chan <moefc32@gmail.com>
2022-10-23 21:09:57 +0700

    tambah daftar repo dari github

--- res/script.js
+++ res/script.js
@@ -2,43 +2,38 @@
 
 const tab = document.querySelector('#tab');
 const mobileTab = document.querySelector('#mobile-tab');
+const githubApi = 'https://api.github.com/users/moefc32/repos';
 
-const links = {
-    'CV': [
-        'fa-stream',
-        'https://cv.mf-chan.com'
-    ],
-    'Design': [
-        'fa-folder',
-        'https://design.mf-chan.com'
-    ],
-    'Research': [
-        'fa-search',
-        'https://research.mf-chan.com'
-    ],
-    'Certificate': [
-        'fa-file',
-        'https://certificate.mf-chan.com'
-    ],
-};
+const links = [
+    {
+        name: 'CV',
+        icon: 'fa-stream',
+        link: 'https://cv.mf-chan.com'
+    },
+    {
+        name: 'Design',
+        icon: 'fa-folder',
+        link: 'https://design.mf-chan.com'
+    },
+    {
+        name: 'Research',
+        icon: 'fa-search',
+        link: 'https://research.mf-chan.com'
+    },
+    {
+        name: 'Certificate',
+        icon: 'fa-file',
+        link: 'https://certificate.mf-chan.com'
+    }
+];
 
-function tabInject(title, icon, url) {
-    let item = '<li><a href="' + url + '" target="_blank">\n';
-    item += '<i class="fas ' + icon + ' fa-fw"></i> ' + title + '\n';
-    item += '</a></li>\n';
-
-    tab.innerHTML += item;
-}
-
-function mobileTabInject(title, url) {
-    let item = '<a href="' + url + '" target="_blank">\n';
-    item += '<span>' + title + '</span>\n';
-    item += '</a>\n';
-
-    mobileTab.innerHTML += item;
-}
-
-for (let link in links) {
-    tabInject(link, links[link][0], links[link][1]);
-    mobileTabInject(link, links[link][1]);
+const bodyData = () => {
+    return {
+        links,
+        githubToggle: false,
+        githubRepos: {},
+        async retrieveRepos() {
+            this.githubRepos = await (await fetch(githubApi)).json();
+        }
+    }
 }
