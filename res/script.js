829acc4db195a659ad53396d291bd0b31b246262
Faizal Chan <moefc32@gmail.com>
2022-10-23 22:41:06 +0700

    smooth slide repo list

--- res/script.js
+++ res/script.js
@@ -1,35 +1,63 @@
 'use strict';
 
-const tab = document.querySelector('#tab');
-const mobileTab = document.querySelector('#mobile-tab');
 const githubApi = 'https://api.github.com/users/moefc32/repos';
 
-const links = [
+const subdomains = [
     {
         name: 'CV',
-        icon: 'fa-stream',
+        icon: 'fas fa-stream fa-fw',
         link: 'https://cv.mf-chan.com'
     },
     {
         name: 'Design',
-        icon: 'fa-folder',
+        icon: 'fas fa-folder fa-fw',
         link: 'https://design.mf-chan.com'
     },
     {
         name: 'Research',
-        icon: 'fa-search',
+        icon: 'fas fa-search fa-fw',
         link: 'https://research.mf-chan.com'
     },
     {
         name: 'Certificate',
-        icon: 'fa-file',
+        icon: 'fas fa-file fa-fw',
         link: 'https://certificate.mf-chan.com'
     }
 ];
 
+const hyperlinks = [
+    {
+        icon: 'fab fa-linkedin fa-fw',
+        link: 'https://www.linkedin.com/in/moefc32/'
+    },
+    {
+        icon: 'fab fa-behance fa-fw',
+        link: 'https://behance.net/moefc32'
+    },
+    {
+        icon: 'fab fa-dribbble fa-fw',
+        link: 'https://dribbble.com/moefc32'
+    },
+    {
+        icon: 'fab fa-github fa-fw',
+        link: 'https://github.com/moefc32'
+    },
+    {
+        icon: 'fab fa-medium fa-fw',
+        link: 'https://medium.com/@moefc32'
+    },
+    {
+        icon: 'fas fa-envelope fa-fw',
+        link: 'mailto:hai@mf-chan.com'
+    },
+
+    
+];
+
 const bodyData = () => {
     return {
-        links,
+        subdomains,
+        hyperlinks,
         githubToggle: false,
         githubRepos: {},
         async retrieveRepos() {
