b6cc07130915e374ef0f8390294ce3977f65884c
Faizal Chan. <moefc32@gmail.com>
2024-04-30 02:48:42 +0700

    add new subdomain

--- res/script.js
+++ res/script.js
@@ -3,54 +3,59 @@
 const subdomains = [
     {
         name: 'CV',
-        icon: 'fas fa-stream fa-fw',
+        icon: 'fa-solid fa-stream fa-fw',
         link: 'https://cv.mf-chan.com'
     },
     {
         name: 'Research',
-        icon: 'fas fa-search fa-fw',
+        icon: 'fa-solid fa-search fa-fw',
         link: 'https://research.mf-chan.com'
     },
     {
         name: 'Design',
-        icon: 'fas fa-folder fa-fw',
+        icon: 'fa-solid fa-folder fa-fw',
         link: 'https://design.mf-chan.com'
     },
     {
         name: 'Code',
-        icon: 'fas fa-code fa-fw',
+        icon: 'fa-solid fa-code fa-fw',
         link: 'https://code.mf-chan.com'
     },
     {
         name: 'Certificate',
-        icon: 'fas fa-file fa-fw',
+        icon: 'fa-solid fa-file fa-fw',
         link: 'https://certificate.mf-chan.com'
+    },
+    {
+        name: 'Animanga',
+        icon: 'fa-solid fa-cat fa-fw',
+        link: 'https://animanga.mf-chan.com'
     },
 ];
 
 const hyperlinks = [
     {
-        icon: 'fab fa-linkedin fa-fw',
+        icon: 'fa-brands fa-linkedin fa-fw',
         link: 'https://www.linkedin.com/in/moefc32/'
     },
     {
-        icon: 'fab fa-behance fa-fw',
+        icon: 'fa-brands fa-behance fa-fw',
         link: 'https://behance.net/moefc32'
     },
     {
-        icon: 'fab fa-dribbble fa-fw',
+        icon: 'fa-brands fa-dribbble fa-fw',
         link: 'https://dribbble.com/moefc32'
     },
     {
-        icon: 'fab fa-github fa-fw',
+        icon: 'fa-brands fa-github fa-fw',
         link: 'https://github.com/moefc32'
     },
     {
-        icon: 'fab fa-medium fa-fw',
+        icon: 'fa-brands fa-medium fa-fw',
         link: 'https://medium.com/@moefc32'
     },
     {
-        icon: 'fas fa-envelope fa-fw',
+        icon: 'fa-solid fa-envelope fa-fw',
         link: 'mailto:hai@mf-chan.com'
     },
 
