7b1c53f24bda746d95a5d0be6d7c19ac2acf7ec5
Faizal Chan <moefc32@gmail.com>
2024-11-26 07:34:12 +0700

    add code base

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script>import "../app.css";</script>
+
+<svelte:head>
+  <title>
+    {import.meta.env.VITE_APP_NAME}
+  </title>
+
+  <meta property="og:title" content="{import.meta.env.VITE_APP_NAME}" />
+  <meta property="og:site_name" content="{import.meta.env.VITE_APP_NAME}" />
+  <meta property="og:type" content="website" />
+  <meta property="og:description" content="Faizal Chan. - Strong design management professional with a master of management degree, focused in UX from Telkom University." />
+  <meta property="og:image" content="https://mf-chan.com/og.jpg" />
+
+  <script async src="{&quot;https://www.googletagmanager.com/gtag/js?id=&quot;" + import.meta.env.vite_google_tag}></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+
+    gtag("js", new Date());
+    gtag("config", import.meta.env.VITE_GOOGLE_TAG);
+  </script>
+</svelte:head>
+
+<slot></slot>
