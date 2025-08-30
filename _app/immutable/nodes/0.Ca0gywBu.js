76a9160383f642d213471a07bdecdf0e2ccab7e8
Faizal Chan. <moefc32@gmail.com>
2025-08-30 16:25:56 +0700

    Updates

---
+++ _app/immutable/nodes/0.Ca0gywBu.js
@@ -0,0 +1,12 @@
+import"../chunks/Bzak7iHL.js";import{i as d}from"../chunks/BhWfIHQ7.js";import{w as u,$ as _,a0 as y,a1 as s,a2 as i,A as h,a3 as w,a4 as m,a5 as v,a6 as b,a7 as z,a8 as j}from"../chunks/WPT233Br.js";import{s as C,a as t}from"../chunks/D7FQ60es.js";const F=!0,$=Object.freeze(Object.defineProperty({__proto__:null,prerender:F},Symbol.toStringTag,{value:"Module"}));var U=w(j(`<meta property="og:title"/> <meta property="og:site_name"/> <meta property="og:type" content="website"/> <meta property="og:description" content="Faizal Chan. - Strong design management professional with a master of management degree, focused in UX from Telkom University."/> <meta property="og:image"/> <script src="https://www.googletagmanager.com/gtag/js?id=UA-131278165-1" async><\/script> <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+        gtag('config', 'UA-131278165-1');
+    <\/script>`,1));function x(p,a){u(a,!1);const g="https://mf-chan.com/og.png";d();var e=_();y(l=>{var o=U(),r=s(o),n=m(r,2),f=m(n,6);v(4),b(()=>{z.title=`
+        Faizal Chan's Website
+    `,t(r,"content","Faizal Chan's Website"),t(n,"content","Faizal Chan's Website"),t(f,"content",g)}),i(l,o)});var c=s(e);C(c,a,"default",{}),i(p,e),h()}export{x as component,$ as universal};
