532db816cba7809bc2636984bd3ba172dc091fba
Faizal Chan. <moefc32@gmail.com>
2026-03-24 18:59:54 +0700

    content enhancements

--- src/components/Subdomains.svelte
+++ src/components/Subdomains.svelte
@@ -10,42 +10,41 @@
             <li
                 class="block pb-1 bg-white rounded-t-lg h-[36px] cursor-pointer"
             >
-                <span class="inline-block px-4 py-2">
+                <div class="inline-block px-4 py-2">
                     <svelte:component
                         this={item.icon}
                         size={16}
                         class={'inline-block me-1'}
                     />
                     {item.name}
-                </span>
+                </div>
             </li>
         {:else}
             <li
                 class="block pb-1 bg-gray-200 transition-all duration-250 ease-in-out hover:h-[40px] rounded-t-lg h-[36px] cursor-pointer"
             >
-                <a
-                    href={item.link}
-                    target="_blank"
-                    class="inline-block px-4 py-2"
-                >
-                    <svelte:component
-                        this={item.icon}
-                        size={16}
-                        class={'inline-block me-1'}
-                    />
-                    {item.name}
-                </a>
+                <div class="tooltip" data-tip={item.tooltip}>
+                    <a href={item.link} class="inline-block px-4 py-2">
+                        <svelte:component
+                            this={item.icon}
+                            size={16}
+                            class={'inline-block me-1'}
+                        />
+                        {item.name}
+                    </a>
+                </div>
             </li>
         {/if}
     {/each}
 {:else}
     {#each subdomain as item, i}
-        <a
-            href={item.link}
-            target="_blank"
-            class="block px-6 py-4 bg-slate-500 hover:bg-slate-400 text-white text-center rounded-md"
-        >
-            {item.name}
-        </a>
+        {#if i > 0}
+            <a
+                href={item.link}
+                class="block px-6 py-4 bg-slate-500 hover:bg-slate-400 text-white text-center rounded-md"
+            >
+                {item.name}
+            </a>
+        {/if}
     {/each}
 {/if}
--- src/lib/subdomain.js
+++ src/lib/subdomain.js
@@ -8,26 +8,31 @@ export const subdomain = [
     },
     {
         name: 'Research',
+        tooltip: 'Problem framing',
         icon: Search,
         link: 'https://research.mf-chan.com',
     },
     {
         name: 'Design',
+        tooltip: 'Solution shaping',
         icon: Brush,
         link: 'https://design.mf-chan.com',
     },
     {
         name: 'Code',
+        tooltip: 'Implementation',
         icon: Code,
         link: 'https://code.mf-chan.com',
     },
     {
         name: 'Mentor',
+        tooltip: 'Mentorship activities',
         icon: MessageSquareQuote,
         link: 'https://mentor.mf-chan.com',
     },
     {
         name: 'Certificate',
+        tooltip: 'Verified achievements',
         icon: FileBadge,
         link: 'https://certificate.mf-chan.com',
     },
